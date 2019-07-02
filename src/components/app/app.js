2754220abc7134d983387d2a729f9cdaa3fb1a1d
Yulia Dubenchuk <yulia.dubenchuk@1000geeks.com>
2019-07-02 09:54:49 +0300

    add new component header and table

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import ShopHeader from '../shop-header';
 import { HomePage, CardPage } from '../pages';
 
 import './app.css';
 import { withBookstoreService } from '../hoc';
 
-class App extends Component {
-
-    state = {
-
-    };
-
-    render() {
-        console.log(this.props.bookstoreService.getBooks());
-        return (
-            <div className="container col-lg-10">
+const App = () => {
+    return (
+        <main role="main" className="container">
+            <ShopHeader numItems={5} total={210} />
             <Switch>
                 <Route path="/" component={HomePage}  exact />
                 <Route path="/card" component={CardPage} />
             </Switch>
-            </div>
-        );
-    }
-
+        </main>
+    );
 }
 
 export default withBookstoreService()(App);
